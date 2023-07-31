a5b6b4fe8ebecbbc816f25a2e2107596a8978895
Dmitry Ivanov <dmitry.ivanov@cherehapa.ru>
2023-08-01 00:15:51 +0300

    + argos, authdto etc

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signin() {
-    return { msg: 'signed in!' };
+  constructor(private prisma: PrismaService) {}
+
+  signup(dto: AuthDto) {
+    // generate the password hash
+
+    // save the new user in the db
+
+    //return the saved user
+    return { msg: 'signed up!' };
   }
 
-  signup() {
-    return { msg: 'signed up!' };
+  signin() {
+    return { msg: 'signed in!' };
   }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'mysql://root:password@localhost:3306/nest?schema=public',
+        },
+      },
+    });
+  }
+}
